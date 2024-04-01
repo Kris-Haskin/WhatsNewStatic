6e10ca782b1a1d979fc9fc3c87c8bfe24df48f87
Kris Haskin <viniferouskris@gmail.com>
2024-03-31 22:07:09 -0500

    h1 slideshow titl (pemenane)

--- WhatsNewStatic/functions/fetchNotion.js
+++ WhatsNewStatic/functions/fetchNotion.js
@@ -205,7 +205,7 @@ function generateHTML(data) {
             </style>
         </head>
         <body>
-        <h1>H1 tetset</h1>
+        <h1>Dog Breeds</h1>
            <div id="slide-container">${slidesHTML.join('')}</div>
             <script>
                 // JavaScript code for controlling the slideshow
